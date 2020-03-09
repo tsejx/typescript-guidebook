22e182ae901769377b114df7f46137a3fc678a3d
tsejx <tsejx@foxmail.com>
2020-03-09 23:27:06 +0800

    config: add repository configuration and readme

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export default {
+  mode: 'site',
+  title: 'TypeScript Guidebook',
+  description: 'TypeScript 完全知识体系',
+  base: '/typescript-guidebook/',
+  publicPath: '/typescript-guidebook/',
+  logo: './favicon.svg',
+  favicon: './favicon.svg',
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/tsejx/typescript-guidebook',
+    },
+  ],
+};
